20005d3185f9fe4f4fce8c902801ead37cbf19dc
alex flores <afloresc@fi.uba.ar>
2023-01-19 17:09:11 -0300

    segunda entrega

---
+++ js/productos.js
@@ -0,0 +1,11 @@
+const productos = [
+    {id:01, tipo:"Remera", nombre:"God of War", precio: 1400, foto: "./media/GOW.jpg" },
+    {id:02, tipo:"Remera", nombre:"gta5", precio: 1400, foto: "./media/gta5.jpg" },
+    {id:03, tipo:"Remera", nombre:"untcharted", precio: 1000, foto: "./media/untcharted.jpg" },
+    {id:04, tipo:"Remera", nombre:"gotham", precio: 1000, foto: "./media/gotham.jpg" },
+    {id:05, tipo:"Remera", nombre:"onepiece", precio: 1400, foto: "./media/onepiece.jpg" },
+    {id:06, tipo:"Remera", nombre:"spiderman", precio: 1500, foto: "./media/spiderman.jpg" },
+    {id:07, tipo: "Buzo", nombre: "re2", precio: 4500, foto: "./media/re2.jpg"},
+    {id:08, tipo: "Buzo", nombre: "re4", precio: 3800, foto: "./media/re4.jpg"},
+    {id:09, tipo: "Buzo", nombre: "ac", precio: 3800, foto: "./media/ac.jpg"},
+];
